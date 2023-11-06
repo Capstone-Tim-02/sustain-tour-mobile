99e00bec48da44ea0c6b966f8c976868890aa80d
gabisaCSS <gtrmastah@gmail.com>
2023-11-06 17:28:19 +0700

    font asset poppins

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sustain_tour_mobile/constants/fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,8 +106,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
+              style: TextStyle(
+                  fontFamily: Fonts.poppins,
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.normal),
             ),
             Text(
               '$_counter',
