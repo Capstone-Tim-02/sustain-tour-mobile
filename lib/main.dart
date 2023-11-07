76d3bd6074640e84dcdddc033862747f3e64227f
Muhammad Febi H <febihidayat03@gmail.com>
2023-11-07 14:30:59 +0700

    membuat textField global widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sustain_tour_mobile/style/text_style_widget.dart';
+import 'package:sustain_tour_mobile/febi_coba_fitur/coba_widget_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  CobaWidgetScreen(),
     );
   }
 }
---
+++ lib/widget/text_field_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:sustain_tour_mobile/style/color_theme_style.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? labelText;
+  final String? hintText;
+  final String? errorText;
+  final Icon? prefixIcon;
+  final TextInputType? keyboardType;
+  final TextCapitalization textCapitalization;
+
+  const TextFieldWidget({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    required this.hintText,
+    required this.errorText,
+    required this.prefixIcon,
+    this.keyboardType,
+    this.textCapitalization = TextCapitalization.sentences,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 380,
+      height: 106,
+      child: TextFormField(
+        textCapitalization: textCapitalization,
+        // membuat huruf pertama pada keyboard HP menjadi kapital
+        keyboardType: keyboardType, //mengubah jenis keyboard
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          errorText: errorText,
+          prefixIcon: prefixIcon,
+          enabledBorder: OutlineInputBorder(
+            // Garis border biasa
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              color: ColorThemeStyle.grey100, // Warna garis
+              width: 2.0, // Ketebalan garis
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            // Garis border saat difokuskan
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              color: ColorThemeStyle.blue100, // Warna garis
+              width: 2.0, // Ketebalan garis
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            // Garis border saat terjadi kesalahan penginputan data
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              color: ColorThemeStyle.red, // Warna garis
+              width: 2.0, // Ketebalan garis
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            // Garis border saat terjadi kesalahan penginputan data
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              color: ColorThemeStyle.red, // Warna garis
+              width: 2.0, // Ketebalan garis
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
