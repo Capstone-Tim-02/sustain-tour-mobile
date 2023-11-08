fb2e59b14da29727a98b0d5ca0627bc1d4390742
Muhammad Febi H <febihidayat03@gmail.com>
2023-11-08 21:55:29 +0700

    membuat class assets image pada folder constants

---
+++ lib/constants/assets_image.dart
@@ -0,0 +1,99 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsCardImagesCard1
+  /// assets/card_images/Card1.png
+  static const String assetsCardImagesCard1 = "assets/card_images/Card1.png";
+
+  /// Assets for assetsCardImagesCard2
+  /// assets/card_images/card2.png
+  static const String assetsCardImagesCard2 = "assets/card_images/card2.png";
+
+  /// Assets for assetsCardImagesCard3
+  /// assets/card_images/Card3.png
+  static const String assetsCardImagesCard3 = "assets/card_images/Card3.png";
+
+  /// Assets for assetsIconsCamera
+  /// assets/icons/camera.svg
+  static const String assetsIconsCamera = "assets/icons/camera.svg";
+
+  /// Assets for assetsIconsEmail
+  /// assets/icons/Email.svg
+  static const String assetsIconsEmail = "assets/icons/Email.svg";
+
+  /// Assets for assetsIconsHelp
+  /// assets/icons/Help.svg
+  static const String assetsIconsHelp = "assets/icons/Help.svg";
+
+  /// Assets for assetsIconsHomeFill
+  /// assets/icons/HomeFill.svg
+  static const String assetsIconsHomeFill = "assets/icons/HomeFill.svg";
+
+  /// Assets for assetsIconsInfo
+  /// assets/icons/info.svg
+  static const String assetsIconsInfo = "assets/icons/info.svg";
+
+  /// Assets for assetsIconsKey
+  /// assets/icons/Key.svg
+  static const String assetsIconsKey = "assets/icons/Key.svg";
+
+  /// Assets for assetsIconsLocationFill
+  /// assets/icons/LocationFill.svg
+  static const String assetsIconsLocationFill = "assets/icons/LocationFill.svg";
+
+  /// Assets for assetsIconsLogout
+  /// assets/icons/Logout.svg
+  static const String assetsIconsLogout = "assets/icons/Logout.svg";
+
+  /// Assets for assetsIconsName
+  /// assets/icons/Name.svg
+  static const String assetsIconsName = "assets/icons/Name.svg";
+
+  /// Assets for assetsIconsPhone
+  /// assets/icons/Phone.svg
+  static const String assetsIconsPhone = "assets/icons/Phone.svg";
+
+  /// Assets for assetsIconsPromo
+  /// assets/icons/Promo.svg
+  static const String assetsIconsPromo = "assets/icons/Promo.svg";
+
+  /// Assets for assetsIconsSends
+  /// assets/icons/sends.svg
+  static const String assetsIconsSends = "assets/icons/sends.svg";
+
+  /// Assets for assetsIconsTicketOn
+  /// assets/icons/TicketOn.svg
+  static const String assetsIconsTicketOn = "assets/icons/TicketOn.svg";
+
+  /// Assets for assetsIconsUsername
+  /// assets/icons/Username.svg
+  static const String assetsIconsUsername = "assets/icons/Username.svg";
+
+  /// Assets for assetsIconsUsers
+  /// assets/icons/Users.svg
+  static const String assetsIconsUsers = "assets/icons/Users.svg";
+
+  /// Assets for assetsIconsWisataAlam
+  /// assets/icons/Wisata_Alam.svg
+  static const String assetsIconsWisataAlam = "assets/icons/Wisata_Alam.svg";
+
+  /// Assets for assetsIconsWisataBudaya
+  /// assets/icons/Wisata_Budaya.svg
+  static const String assetsIconsWisataBudaya = "assets/icons/Wisata_Budaya.svg";
+
+  /// Assets for assetsIconsWisataHiburan
+  /// assets/icons/Wisata_Hiburan.svg
+  static const String assetsIconsWisataHiburan = "assets/icons/Wisata_Hiburan.svg";
+
+  /// Assets for assetsIconsWisataSejarah
+  /// assets/icons/Wisata_Sejarah.svg
+  static const String assetsIconsWisataSejarah = "assets/icons/Wisata_Sejarah.svg";
+
+  /// Assets for assetsImagesLogoDestimate
+  /// assets/images/logo_destimate.png
+  static const String assetsImagesLogoDestimate = "assets/images/logo_destimate.png";
+
+  /// Assets for assetsImagesOnboarding
+  /// assets/images/onboarding.png
+  static const String assetsImagesOnboarding = "assets/images/onboarding.png";
+}
