85ac6871bd271167f290c153ff72b792ce910dea
StevenOscar <stevenoscar458@gmail.com>
2023-11-13 12:24:48 +0700

    pull dari development terbaru

--- lib/screen/explore_screen/explore_screen.dart
+++ lib/screen/explore_screen/explore_screen.dart
@@ -80,7 +80,7 @@ class _ExploreScreenState extends State<ExploreScreen> {
                   imageUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Pemandangan_Gunung_Bromo.jpg",
                   title: "Wisata alam",
                   location: "Surabaya",
-                  subtitle: "Rp 200.000"
+                  price : 200000
                 );
               },
             ),
