8fa4208dfc5c0ddea6b7b1ba4e7bb0b6d7edc533
StevenOscar <stevenoscar458@gmail.com>
2023-11-26 18:01:42 +0700

    minor changes main screen, explore, dan home

--- lib/screen/explore_screen/components/title_explore.dart
+++ lib/screen/explore_screen/components/title_explore.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:sustain_tour_mobile/style/color_theme_style.dart';
+import 'package:sustain_tour_mobile/style/font_weight_style.dart';
 import 'package:sustain_tour_mobile/style/text_style_widget.dart';
 
-class TitleExplore extends StatelessWidget {
-  const TitleExplore({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40, bottom: 4),
-      child: Text(
-        "Explore",
-        style: TextStyleWidget.titleT2(
-          color: ColorThemeStyle.black100,
-          fontWeight: FontWeight.w600
-        ),
+class TitleExplore {
+  static AppBar exploreAppBar = AppBar(
+    title: Text(
+      'Explore',
+      style: TextStyleWidget.titleT2(
+        fontWeight: FontWeightStyle.semiBold,
       ),
-    );
-  }
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    surfaceTintColor: Colors.white,
+  );
 }
