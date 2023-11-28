cfd6c5a1b22c925f4a57e84825386f5bf8bebe30
Muhammad Febi H <febihidayat03@gmail.com>
2023-11-28 12:08:14 +0700

    memperbaiki issue pada desti point
    memperbaiki issue pada desti point saat aplikasi di jalankan pertama kali maka data yang tampil adalah point anda kosong, tidak melakukan pengecekan data dulu, sekarang melakukan pengecekan data dulu baru memunculkan data ada atau tidak

--- lib/screen/home_screen/components/desti_point_screen/component/point_history.dart
+++ lib/screen/home_screen/components/desti_point_screen/component/point_history.dart
@@ -14,127 +14,118 @@ class PointHistory extends StatelessWidget {
     final pointHistoryProvider = context.read<DestiPointProvider>();
     pointHistoryProvider.getPointHistory();
     return Expanded(
-      child: pointHistoryProvider.listPointHistory.isNotEmpty
-          ? Consumer<DestiPointProvider>(
-              builder: (context, destiPointProvider, child) {
-                return destiPointProvider.isLoadingHistory == true
-                    ? const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : ListView.builder(
-                        itemCount: destiPointProvider.listPointHistory.length,
-                        itemBuilder: (context, index) {
-                          var dataPointHistory =
-                              destiPointProvider.listPointHistory[index];
-                          //pengecekan apakah data point Earned ada atau tidak
-                          String pointsEarnedText =
-                              dataPointHistory.pointsEarned != null
-                                  ? 'Points Earned'
-                                  : '';
+      child: Consumer<DestiPointProvider>(
+        builder: (context, destiPointProvider, child) {
+          if (destiPointProvider.isLoadingHistory) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (destiPointProvider.listPointHistory.isNotEmpty) {
+            return ListView.builder(
+              itemCount: destiPointProvider.listPointHistory.length,
+              itemBuilder: (context, index) {
+                var dataPointHistory =
+                    destiPointProvider.listPointHistory[index];
+                //pengecekan apakah data point Earned ada atau tidak
+                String pointsEarnedText = dataPointHistory.pointsEarned != null
+                    ? 'Points Earned'
+                    : '';
 
-                          //pengecekan apakah data point Used ada atau tidak
-                          String pointsUsedText =
-                              dataPointHistory.pointsUsed != null
-                                  ? 'Points Used'
-                                  : '';
+                //pengecekan apakah data point Used ada atau tidak
+                String pointsUsedText =
+                    dataPointHistory.pointsUsed != null ? 'Points Used' : '';
 
-                          // Tampilkan hanya satu dari dua teks berdasarkan ketersediaan data
-                          String displayText = pointsEarnedText.isNotEmpty
-                              ? pointsEarnedText
-                              : (pointsUsedText.isNotEmpty
-                                  ? pointsUsedText
-                                  : '');
+                // Tampilkan hanya satu dari dua teks berdasarkan ketersediaan data
+                String displayText = pointsEarnedText.isNotEmpty
+                    ? pointsEarnedText
+                    : (pointsUsedText.isNotEmpty ? pointsUsedText : '');
 
-                          String pointsEarned =
-                              dataPointHistory.pointsEarned != null
-                                  ? '+ ${dataPointHistory.pointsEarned}'
-                                  : '';
+                String pointsEarned = dataPointHistory.pointsEarned != null
+                    ? '+ ${dataPointHistory.pointsEarned}'
+                    : '';
 
-                          //pengecekan apakah data point Used ada atau tidak
-                          String pointsUsed =
-                              dataPointHistory.pointsUsed != null
-                                  ? '- ${dataPointHistory.pointsUsed}'
-                                  : '';
+                //pengecekan apakah data point Used ada atau tidak
+                String pointsUsed = dataPointHistory.pointsUsed != null
+                    ? '- ${dataPointHistory.pointsUsed}'
+                    : '';
 
-                          // Menentukan warna teks berdasarkan apakah yang ditampilkan adalah pointsEarned atau pointsUsed
-                          Color textColor = pointsEarned.isNotEmpty
-                              ? ColorThemeStyle.green100
-                              : (pointsUsed.isNotEmpty
-                                  ? ColorThemeStyle.red
-                                  : ColorThemeStyle.black100);
+                // Menentukan warna teks berdasarkan apakah yang ditampilkan adalah pointsEarned atau pointsUsed
+                Color textColor = pointsEarned.isNotEmpty
+                    ? ColorThemeStyle.green100
+                    : (pointsUsed.isNotEmpty
+                        ? ColorThemeStyle.red
+                        : ColorThemeStyle.black100);
 
-                          Color containerColor = pointsEarned.isNotEmpty
-                              ? ColorThemeStyle.greenContainer
-                              : (pointsUsed.isNotEmpty
-                                  ? ColorThemeStyle.redContainer
-                                  : ColorThemeStyle.black100);
+                Color containerColor = pointsEarned.isNotEmpty
+                    ? ColorThemeStyle.greenContainer
+                    : (pointsUsed.isNotEmpty
+                        ? ColorThemeStyle.redContainer
+                        : ColorThemeStyle.black100);
 
-                          // Tampilkan hanya satu dari dua teks berdasarkan ketersediaan data
-                          String displayPoint = pointsEarned.isNotEmpty
-                              ? pointsEarned
-                              : (pointsUsed.isNotEmpty ? pointsUsed : '');
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: Container(
-                              width: 380,
-                              height: 64,
-                              decoration: BoxDecoration(
-                                color: ColorThemeStyle.white100,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: ColorThemeStyle.black100
-                                        .withOpacity(0.1),
-                                    spreadRadius: 0,
-                                    blurRadius: 10,
-                                    offset: const Offset(0, 0),
-                                  ),
-                                ],
-                              ),
-                              child: ListTile(
-                                contentPadding: const EdgeInsets.only(
-                                  right: 8,
-                                  left: 8,
-                                ),
-                                title: Text(
-                                  dataPointHistory.wisataName ?? "",
-                                  style: TextStyleWidget.titleT2(
-                                    fontWeight: FontWeightStyle.medium,
-                                    color: ColorThemeStyle.black100,
-                                  ),
-                                ),
-                                subtitle: Text(
-                                  displayText,
-                                  style: TextStyleWidget.bodyB3(
-                                    fontWeight: FontWeightStyle.medium,
-                                    color: ColorThemeStyle.grey100,
-                                  ),
-                                ),
-                                trailing: Container(
-                                  width: 54,
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(6),
-                                    color: containerColor,
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      displayPoint,
-                                      style: TextStyleWidget.bodyB2(
-                                        fontWeight: FontWeightStyle.semiBold,
-                                        color: textColor,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
+                // Tampilkan hanya satu dari dua teks berdasarkan ketersediaan data
+                String displayPoint = pointsEarned.isNotEmpty
+                    ? pointsEarned
+                    : (pointsUsed.isNotEmpty ? pointsUsed : '');
+                return Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Container(
+                    width: 380,
+                    height: 64,
+                    decoration: BoxDecoration(
+                      color: ColorThemeStyle.white100,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: ColorThemeStyle.black100.withOpacity(0.1),
+                          spreadRadius: 0,
+                          blurRadius: 10,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.only(
+                        right: 8,
+                        left: 8,
+                      ),
+                      title: Text(
+                        dataPointHistory.wisataName ?? "",
+                        style: TextStyleWidget.titleT2(
+                          fontWeight: FontWeightStyle.medium,
+                          color: ColorThemeStyle.black100,
+                        ),
+                      ),
+                      subtitle: Text(
+                        displayText,
+                        style: TextStyleWidget.bodyB3(
+                          fontWeight: FontWeightStyle.medium,
+                          color: ColorThemeStyle.grey100,
+                        ),
+                      ),
+                      trailing: Container(
+                        width: 54,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          color: containerColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            displayPoint,
+                            style: TextStyleWidget.bodyB2(
+                              fontWeight: FontWeightStyle.semiBold,
+                              color: textColor,
                             ),
-                          );
-                        },
-                      );
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               },
-            )
-          : Column(
+            );
+          } else {
+            return Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -158,7 +149,10 @@ class PointHistory extends StatelessWidget {
                   ),
                 ),
               ],
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
