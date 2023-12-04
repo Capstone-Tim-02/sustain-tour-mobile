d8a1ee5d4388beeec72faf04c3282b163efe9cc4
gabisaCSS <gtrmastah@gmail.com>
2023-12-04 20:29:17 +0700

    hapus booking history api dan konek history succes ke detial transaksi

--- lib/screen/profile_screen/component/profile_emission_component/component/detail_emission_screen_component/component/travel_history_component/component/travel_history_list_component/travel_history_list_component.dart
+++ lib/screen/profile_screen/component/profile_emission_component/component/detail_emission_screen_component/component/travel_history_component/component/travel_history_list_component/travel_history_list_component.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sustain_tour_mobile/constants/assets_image.dart';
+import 'package:sustain_tour_mobile/constants/routes.dart';
 import 'package:sustain_tour_mobile/models/booking_models/booking_history_model.dart';
 import 'package:sustain_tour_mobile/screen/profile_screen/component/profile_emission_component/component/detail_emission_screen_component/component/travel_history_component/travel_history_provider.dart';
 import 'package:sustain_tour_mobile/style/color_theme_style.dart';
@@ -32,120 +33,130 @@ class TravelHistoryListComponent extends StatelessWidget {
                       ticketDataList[ticketDataList.length - 1 - index];
 
                   if (ticket.statusOrder == 'success') {
-                    return Container(
-                      height: 81,
-                      width: double.infinity,
-                      margin: const EdgeInsets.only(
-                        bottom: 16,
-                        right: 16,
-                        left: 16,
+                    return GestureDetector(
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        Routes.detailTransaksiScreen,
+                        arguments: ticket,
                       ),
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 10,
-                              color: Colors.black.withOpacity(0.1),
-                            )
-                          ]),
-                      child: Stack(
-                        alignment: Alignment.bottomRight,
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                width: 82,
-                                height: 65,
-                                decoration: const BoxDecoration(
-                                    color: ColorThemeStyle.blue40,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(10))),
-                                child: ClipRRect(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10)),
-                                  child: Image.network(
-                                    ticket.photoWisata1,
-                                    fit: BoxFit.fill,
-                                    errorBuilder: (context, error, stackTrace) {
-                                      return Image.network(
-                                        ticket.photoWisata2,
-                                        fit: BoxFit.fill,
-                                        errorBuilder:
-                                            (context, error, stackTrace) {
-                                          return Image.network(
-                                            ticket.photoWisata3,
-                                            fit: BoxFit.fill,
-                                            errorBuilder:
-                                                (context, error, stackTrace) =>
-                                                    const Center(
-                                                        child: Icon(
-                                              Icons
-                                                  .image_not_supported_outlined,
-                                              // size: 50,
-                                            )),
-                                          );
-                                        },
-                                      );
-                                    },
+                      child: Container(
+                        height: 81,
+                        width: double.infinity,
+                        margin: const EdgeInsets.only(
+                          bottom: 16,
+                          right: 16,
+                          left: 16,
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                              )
+                            ]),
+                        child: Stack(
+                          alignment: Alignment.bottomRight,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 82,
+                                  height: 65,
+                                  decoration: const BoxDecoration(
+                                      color: ColorThemeStyle.blue40,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(10)),
+                                    child: Image.network(
+                                      ticket.photoWisata1,
+                                      fit: BoxFit.fill,
+                                      errorBuilder:
+                                          (context, error, stackTrace) {
+                                        return Image.network(
+                                          ticket.photoWisata2,
+                                          fit: BoxFit.fill,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return Image.network(
+                                              ticket.photoWisata3,
+                                              fit: BoxFit.fill,
+                                              errorBuilder: (context, error,
+                                                      stackTrace) =>
+                                                  const Center(
+                                                      child: Icon(
+                                                Icons
+                                                    .image_not_supported_outlined,
+                                                // size: 50,
+                                              )),
+                                            );
+                                          },
+                                        );
+                                      },
+                                    ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                width: 21,
-                              ),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    FittedBox(
-                                      fit: BoxFit.scaleDown,
-                                      child: Text(
-                                        ticket.wisataName,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyleWidget.titleT2(
-                                          fontWeight: FontWeightStyle.semiBold,
+                                const SizedBox(
+                                  width: 21,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      FittedBox(
+                                        fit: BoxFit.scaleDown,
+                                        child: Text(
+                                          ticket.wisataName,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyleWidget.titleT2(
+                                            fontWeight:
+                                                FontWeightStyle.semiBold,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Text(
-                                      ticket.kotaWisata,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyleWidget.bodyB3(
-                                        color: ColorThemeStyle.grey100,
-                                        fontWeight: FontWeightStyle.medium,
+                                      Text(
+                                        ticket.kotaWisata,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyleWidget.bodyB3(
+                                          color: ColorThemeStyle.grey100,
+                                          fontWeight: FontWeightStyle.medium,
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                ticket.carboonFootprint.toStringAsFixed(0),
-                                style: TextStyleWidget.bodyB1(
-                                  fontWeight: FontWeightStyle.semiBold,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 4,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 6.0),
-                                child: Text(
-                                  'CO2',
-                                  style: TextStyleWidget.bodyB3(
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  ticket.carboonFootprint.toStringAsFixed(0),
+                                  style: TextStyleWidget.bodyB1(
                                     fontWeight: FontWeightStyle.semiBold,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 6.0),
+                                  child: Text(
+                                    'CO2',
+                                    style: TextStyleWidget.bodyB3(
+                                      fontWeight: FontWeightStyle.semiBold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   } else {
