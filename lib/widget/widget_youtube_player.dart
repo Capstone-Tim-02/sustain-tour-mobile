3d54b0d1b9eb4f2052366c706a6bf539e8598433
DwiWahyu1204 <133838235+DwiWahyuu1204@users.noreply.github.com>
2023-12-12 21:26:42 +0700

    marge widget youtube player

---
+++ lib/widget/widget_youtube_player.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+class YoutubePlayerWidget extends StatelessWidget {
+  final String? linkVid;
+  const YoutubePlayerWidget({super.key, required this.linkVid});
+
+  @override
+  Widget build(BuildContext context) {
+    final videoURL = linkVid ?? "https://www.youtube.com/watch?v=ts8i-6AtDfc";
+    final videoID =
+        YoutubePlayerController.convertUrlToId(videoURL) ?? "InvalidID";
+
+    final youtubeController = YoutubePlayerController.fromVideoId(
+      videoId: videoID,
+      autoPlay: true,
+      params: const YoutubePlayerParams(
+        mute: false,
+        showControls: true,
+        showFullscreenButton: true,
+      ),
+    );
+    return Container(
+      height: 202,
+      width: 380,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: YoutubePlayer(
+        controller: youtubeController,
+        aspectRatio: 16 / 9,
+      ),
+    );
+  }
+}
