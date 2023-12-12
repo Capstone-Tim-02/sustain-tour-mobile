9258afb0288e6c1e2bbd0c0d19e667cc8836048b
Muhammad Febi H <febihidayat03@gmail.com>
2023-12-12 18:56:08 +0700

    membuat youtube player widget

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Public\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\msib\capstune\sustain-tour-mobile"
+export "FLUTTER_ROOT=C:\Users\62896\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\GitHub\sustain-tour-mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
